65c87e99675210f0668b4ee2ebe209e156bb3146
Максим <@DESKTOP-9JOEI7F>
2023-03-21 21:11:40 +0200

    Добавьте файлы проекта.

---
+++ Matrix trace/Matrix trace.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <ctime>
+void matrix()
+{
+    srand(time(0));
+    int strings, columns, Matrix_trace = 0;
+    std::cout << "Enter strings: ";
+    std::cin >> strings;
+    std::cout << "Enter columns: ";
+    std::cin >> columns;
+    int **table = new int* [strings];
+    for (int i = 0; i < strings; i++)
+    {
+        table[i] = new int[columns];
+    }
+    for (int i = 0; i < strings; i++)
+    {
+        std::cout << std::endl;
+        for (int j = 0; j < columns; j++)
+        {
+            int random = rand() % 100;
+            table[i][j] = random;
+            std::cout << table[i][j] << " ";
+            if (i == j) Matrix_trace += table[i][j];
+        }
+
+    }
+    for (int i = 0; i < strings; i++)
+    {
+        delete[] table[i];
+    }
+    delete[] table;
+    std::cout << std::endl;
+    std::cout << "Matrix trace - " << Matrix_trace << std::endl;
+}
+int main()
+{
+    matrix();
+}
